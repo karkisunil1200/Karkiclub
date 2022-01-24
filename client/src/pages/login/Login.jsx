6a43efbce64ef3301163b847c893170d8574ba0d
sunilkarki <karkisunil1200@gmail.com>
2022-01-24 16:22:55 -0600

    Login form hooked up with normal useRef

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,6 +1,16 @@
+import { useRef } from "react";
+
 import "./login.css";
 
 const Login = () => {
+  const email = useRef();
+  const password = useRef();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(email.current.value);
+  };
+
   return (
     <div>
       <div className="login">
@@ -12,15 +22,28 @@ const Login = () => {
             </span>
           </div>
           <div className="loginRight">
-            <div className="loginBox">
-              <input placeholder="Email" className="loginInput" />
-              <input placeholder="Password" className="loginInput" />
+            <form className="loginBox" onSubmit={handleClick}>
+              <input
+                placeholder="Email"
+                type="email"
+                ref={email}
+                className="loginInput"
+                required
+              />
+              <input
+                placeholder="Password"
+                type="password"
+                minLength="6"
+                className="loginInput"
+                ref={password}
+                required
+              />
               <button className="loginButton">Log In</button>
               <span className="loginForgot">Forgot Password?</span>
               <button className="loginRegisterButton">
                 Create New Account
               </button>
-            </div>
+            </form>
           </div>
         </div>
       </div>
