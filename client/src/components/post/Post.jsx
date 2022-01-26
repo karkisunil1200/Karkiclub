d46396f09c439a882c14a0f2d1cc61181d31696e
sunilkarki <karkisunil1200@gmail.com>
2022-01-26 09:47:47 -0600

    Link working on newfeed profile pic

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -41,7 +41,7 @@ const Post = ({ post }) => {
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <Link to={`profile/${user.username}`}>
+            <Link to={`/profile/${user.username}`}>
               <img
                 className="postProfileImg"
                 src={
